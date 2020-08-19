741784e745857ca59ceaf9937a328f1eb2582365
amrebrahem226 <amrebrahem226@gmail.com>
2020-08-19 11:23:28 +0200

    finish the home sliders

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_travel_ui_starter/models/destination_model.dart';
+import 'package:flutter_travel_ui_starter/widgets/destination_carousel.dart';
+import 'package:flutter_travel_ui_starter/widgets/hotel_carousel.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -74,8 +77,156 @@ class _HomeScreenState extends State<HomeScreen> {
             SizedBox(
               height: 20.0,
             ),
+
+            DestinationCarousel(),
+
+            Container(
+              height: 300.0,
+              child: ListView.builder(
+                  itemCount: destinations.length,
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (BuildContext context, int index) {
+                    Destination destination = destinations[index];
+                    return Container(
+                      margin: EdgeInsets.all(10.0),
+                      width: 210.0,
+                      child: Stack(
+                        alignment: Alignment.topCenter,
+                        children: <Widget>[
+                          Positioned(
+                              bottom: 10.0,
+                              child: Container(
+                                height: 120.0,
+                                width: 200.0,
+                                decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    borderRadius: BorderRadius.circular(10.0)),
+                                child: Padding(
+                                  padding: EdgeInsets.all(10.0),
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.end,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: <Widget>[
+                                      Text(
+                                        "${destination.activities.length} Activities",
+                                        style: TextStyle(
+                                          fontSize: 22.0,
+                                          fontWeight: FontWeight.w600,
+                                          letterSpacing: 1.2,
+                                        ),
+                                      ),
+                                      Text(
+                                        destination.description,
+                                        style: TextStyle(
+                                          color: Colors.grey,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              )),
+                          Container(
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(20.0),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.black26,
+                                  offset: Offset(0.0, 2.0),
+                                  blurRadius: 6.0,
+                                ),
+                              ],
+                            ),
+                            child: Stack(
+                              children: <Widget>[
+                                ClipRRect(
+                                  borderRadius: BorderRadius.circular(20.0),
+                                  child: Image(
+                                    width: 180.0,
+                                    height: 175.0,
+                                    image: AssetImage(destination.imageUrl),
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                                Positioned(
+                                    left: 10.0,
+                                    bottom: 10.0,
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: <Widget>[
+                                        Text(destination.city,
+                                            style: TextStyle(
+                                                fontSize: 24.0,
+                                                color: Colors.white,
+                                                fontWeight: FontWeight.w600,
+                                                letterSpacing: 1.2)),
+                                        Row(
+                                          children: <Widget>[
+                                            Icon(
+                                              FontAwesomeIcons.locationArrow,
+                                              size: 10.0,
+                                              color: Colors.white,
+                                            ),
+                                            SizedBox(
+                                              width: 5.0,
+                                            ),
+                                            Text(
+                                              destination.country,
+                                              style: TextStyle(
+                                                color: Colors.white,
+                                              ),
+                                            )
+                                          ],
+                                        ),
+                                      ],
+                                    )),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  }),
+            ),
+
+            SizedBox(
+              height: 5.0,
+            ),
+            HotelTitle(),
+            HotelCarousel(),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class HotelTitle extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 20.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Text("Exclusive Hotels",
+              style: TextStyle(
+                  fontSize: 22.0,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 1.5)),
+          GestureDetector(
+              onTap: () {
+                print("See All");
+              },
+              child: Text("See All",
+                  style: TextStyle(
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.w600,
+                      letterSpacing: 1.0,
+                      color: Theme.of(context).primaryColor))),
+        ],
       ),
     );
   }
